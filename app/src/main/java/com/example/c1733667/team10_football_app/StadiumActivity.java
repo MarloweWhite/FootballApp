34d593866fb54e0d0ec983b2c110428b8667e9a3
James Buckland <bucklandj2@cardiff.ac.uk>
2018-03-13 11:55:35 +0000

    added gradle build

---
+++ app/src/main/java/com/example/c1733667/team10_football_app/StadiumActivity.java
@@ -0,0 +1,13 @@
+package com.example.c1733667.team10_football_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StadiumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stadium);
+    }
+}
