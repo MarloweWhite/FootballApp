dd01020d79c0c219cc0a3a15d0d3e7c2fe117266
James Buckland <bucklandj2@cardiff.ac.uk>
2018-03-13 13:24:37 +0000

    added list of leagues

--- app/src/main/java/com/example/c1733667/team10_football_app/StadiumActivity.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/StadiumActivity.java
@@ -1,13 +1,36 @@
 package com.example.c1733667.team10_football_app;
 
+import android.support.v7.widget.ListViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-public class StadiumActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class StadiumActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private String[] leaugueArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stadium);
+
+        ArrayAdapter<String> adapter;
+        leaugueArray = getResources().getStringArray(R.array.football_leagues);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, leaugueArray);
+
+        ListViewCompat lv = findViewById(R.id.list_view);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(this, String.format("Item clicked on = %d"), Toast.LENGTH_SHORT).show();
     }
 }
+
