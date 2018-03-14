fa9a0bcf422a1fb1a91678b74cb9c524e1d41f13
James Buckland <bucklandj2@cardiff.ac.uk>
2018-03-14 12:17:51 +0000

    able to make header in info activity to change depending on the club slected

--- app/src/main/java/com/example/c1733667/team10_football_app/InfoActivity.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/InfoActivity.java
@@ -1,13 +1,23 @@
 package com.example.c1733667.team10_football_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
+    private TextView clubName;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        clubName= findViewById(R.id.clubName);
+        String clubFromOtherActivity = this.getIntent().getStringExtra("Club Name");
+        if (clubFromOtherActivity != null){
+            clubName.setText(clubFromOtherActivity);
+        }
     }
 }
