fc4578a525ae7175dbd11b7509dafc93045d536c
James Buckland <bucklandj2@cardiff.ac.uk>
2018-03-15 14:47:30 +0000

    added json files and added toolbar

--- app/src/main/java/com/example/c1733667/team10_football_app/InfoActivity.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/InfoActivity.java
@@ -1,13 +1,17 @@
 package com.example.c1733667.team10_football_app;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
     private TextView clubName;
     private Intent intent;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +19,12 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         clubName= findViewById(R.id.clubName);
+        toolbar = findViewById(R.id.infoToolbar);
         String clubFromOtherActivity = this.getIntent().getStringExtra("Club Name");
         if (clubFromOtherActivity != null){
             clubName.setText(clubFromOtherActivity);
+            toolbar.setTitle(clubFromOtherActivity);
+
         }
     }
 }
