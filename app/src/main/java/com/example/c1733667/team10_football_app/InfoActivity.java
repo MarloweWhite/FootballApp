0c9763e6a492541e19ebace16f70ac7db1742650
James Buckland <bucklandj2@cardiff.ac.uk>
2018-03-16 10:33:00 +0000

    populated championleague json file and made info page read from json file

--- app/src/main/java/com/example/c1733667/team10_football_app/InfoActivity.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/InfoActivity.java
@@ -1,14 +1,24 @@
 package com.example.c1733667.team10_football_app;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class InfoActivity extends AppCompatActivity {
-    private TextView clubName;
+    private TextView clubInfo;
     private Intent intent;
     private Toolbar toolbar;
 
@@ -18,13 +28,59 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        clubName= findViewById(R.id.clubName);
+        clubInfo = findViewById(R.id.clubName);
         toolbar = findViewById(R.id.infoToolbar);
         String clubFromOtherActivity = this.getIntent().getStringExtra("Club Name");
-        if (clubFromOtherActivity != null){
-            clubName.setText(clubFromOtherActivity);
+        if (clubFromOtherActivity != null) {
+//            clubInfo.setText(clubFromOtherActivity);
             toolbar.setTitle(clubFromOtherActivity);
+            getClubInfo();
 
         }
     }
+
+    public void getClubInfo() {
+        Resources res = getResources();
+        InputStream is = res.openRawResource(R.raw.championleague);
+        Scanner scanner = new Scanner(is);
+        StringBuilder builder = new StringBuilder();
+        while (scanner.hasNextLine()) {
+            builder.append(scanner.nextLine());
+        }
+        parseJson(builder.toString());
+    }
+
+    private void parseJson(String json) {
+        clubInfo = (TextView) findViewById(R.id.clubName);
+        String clubFromOtherActivity = this.getIntent().getStringExtra("Club Name");
+        StringBuilder builder = new StringBuilder();
+        try {
+            JSONObject root = new JSONObject(json);
+            JSONObject clubInfo = root.getJSONObject(clubFromOtherActivity);
+
+            builder.append("Name: ")
+                    .append(clubInfo.getString("club name"))
+                    .append("\n")
+                    .append("Stadium: ")
+                    .append(clubInfo.getString("Stadium "))
+                    .append("\n")
+                    .append("Stadium Location: ")
+                    .append(clubInfo.getString("Stadium Location"))
+                    .append("\n")
+                    .append("Stadium capacity: ")
+                    .append(clubInfo.getString("Stadium capacity"))
+                    .append("\n")
+                    .append("Manager: ")
+                    .append(clubInfo.getString("Manager"))
+                    .append("\n")
+                    .append("History: ")
+                    .append(clubInfo.getString("History"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        clubInfo.setText(builder.toString());
+    }
+
 }
