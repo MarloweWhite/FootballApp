fce9f43146cc54f37d054671ccbc38cdd2a003eb
Marlowe White <whitem6@cardiff.ac.uk>
2018-03-21 18:37:43 +0000

    added the bar to all, will make clickable and link to each other on 3/22/18

--- app/src/main/java/com/example/c1733667/team10_football_app/MainActivity.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/MainActivity.java
@@ -3,9 +3,17 @@ package com.example.c1733667.team10_football_app;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.support.design.widget.NavigationView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -14,20 +22,43 @@ import com.google.android.gms.ads.MobileAds;
 import java.util.Map;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Button btnStadium;
     private Button btnScore;
     private Button btnMap;
     private Intent intent;
     private AdView mAdView;
+    private DrawerLayout navDrawer;
+    private NavigationView navView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //event handling thing majig
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_outer);
         stadiumButtonListener();
         scoreButtonListener();
         mapbuttonListener();
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        this.navDrawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,navDrawer,toolbar,R.string.open,R.string.close);
+        navDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+        this.navView = findViewById(R.id.nav_view);
+        this.navView.setNavigationItemSelectedListener(this);
         MobileAds.initialize(this, "ca-app-pub-2747796691426534~5751833757");
 
         mAdView = findViewById(R.id.adView);
@@ -66,5 +97,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        this.navDrawer.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
--- app/src/main/java/com/example/c1733667/team10_football_app/StadiumActivity.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/StadiumActivity.java
@@ -1,25 +1,49 @@
 package com.example.c1733667.team10_football_app;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.ListViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
+import android.support.design.widget.NavigationView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class StadiumActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class StadiumActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, NavigationView.OnNavigationItemSelectedListener {
     private String[] leaugueArray;
     private Intent intent;
+    private DrawerLayout navDrawer;
+    private NavigationView navView;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //event handling thing majig
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stadium);
+        setContentView(R.layout.activity_stadium_outer);
 
         ArrayAdapter<String> adapter;
         leaugueArray = getResources().getStringArray(R.array.football_leagues);
@@ -28,6 +52,15 @@ public class StadiumActivity extends AppCompatActivity implements AdapterView.On
         ListViewCompat lv = findViewById(R.id.list_view);
         lv.setAdapter(adapter);
         lv.setOnItemClickListener(this);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        this.navDrawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,navDrawer,toolbar,R.string.open,R.string.close);
+        navDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+        this.navView = findViewById(R.id.nav_view);
+        this.navView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -49,6 +82,16 @@ public class StadiumActivity extends AppCompatActivity implements AdapterView.On
             intent = new Intent(getApplicationContext(),LeagueTwo.class);
             startActivity(intent);
         }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        this.navDrawer.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
 
