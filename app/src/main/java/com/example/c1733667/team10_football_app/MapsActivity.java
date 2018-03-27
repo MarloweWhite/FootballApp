20bfa83f3980cd05418247ab165262bf33d86c08
James Buckland <bucklandj2@cardiff.ac.uk>
2018-03-27 09:57:56 +0100

    added toolbar to map activity

--- app/src/main/java/com/example/c1733667/team10_football_app/MapsActivity.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/MapsActivity.java
@@ -2,10 +2,16 @@ package com.example.c1733667.team10_football_app;
 
 import android.content.SharedPreferences;
 import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
-
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,13 +24,11 @@ import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
 
     private GoogleMap mMap;
     private String[] championLeague;
@@ -33,41 +37,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private String[] leagueTwo;
     private ArrayList<String> clubName;
     private ArrayList<LatLng> visitedClubs;
-    private HashMap<Integer, Boolean> visibility;
-    private ArrayList<Collection> getVisibility;
+    private DrawerLayout navDrawer;
+    private NavigationView navView;
     private SharedPreferences pref1;
     private SharedPreferences pref2;
     private SharedPreferences pref3;
     private SharedPreferences pref4;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        //event handling thing majig
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps_outer);
         visitedClubs = new ArrayList<>();
         clubName = new ArrayList<>();
-        visibility = new HashMap<>();
-        //setting size of arraylist
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        this.navDrawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,navDrawer,toolbar,R.string.open,R.string.close);
+        navDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+        this.navView = findViewById(R.id.nav_view);
+        this.navView.setNavigationItemSelectedListener(this);
+
         pref1 = getSharedPreferences("ChampionPreference", 0);
         pref2 = getSharedPreferences("PremierPreference", 0);
         pref3 = getSharedPreferences("LeagueOnePreference", 0);
         pref4 = getSharedPreferences("LeagueTwoPreference", 0);
-
-        Map map = pref1.getAll();
-        Map map1 = pref2.getAll();
-        Map map2 = pref3.getAll();
-        Map map3 = pref4.getAll();
-        Log.d("pref1 size", String.valueOf(map.size()));
-        Log.d("pref1 vlaues", String.valueOf(map.values()));
-        Log.d("pref1 keyset", String.valueOf(map.keySet()));
-        Log.d("pref2 size", String.valueOf(map1.size()));
-        Log.d("pref2 keyset", String.valueOf(map1.keySet()));
-        Log.d("pref2 vlaues", String.valueOf(map1.values()));
-        Log.d("pref3 size", String.valueOf(map2.size()));
-        Log.d("pref3 vlaues", String.valueOf(map2.values()));
-        Log.d("pref4 size", String.valueOf(map3.size()));
-        Log.d("pref4 vlaues", String.valueOf(map3.values()));
-
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -214,5 +223,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultMap, 6));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
