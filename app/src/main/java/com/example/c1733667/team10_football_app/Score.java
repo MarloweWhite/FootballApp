ba2e85f37f3a19a5d1b41c9bbc4828ab3cb97f51
Pedro Gomes <GomesPO@cardiff.ac.uk>
2018-04-07 18:11:37 +0100

    Scoring system with a rigns for each league and bar for overall progress

--- app/src/main/java/com/example/c1733667/team10_football_app/Score.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/Score.java
@@ -20,6 +20,11 @@ public class Score extends AppCompatActivity implements AdapterView.OnClickListe
 
     private int mainScore;
     ProgressBar mprogressBar;
+    ProgressBar mprogressBar2;
+    ProgressBar mprogressBar3;
+    ProgressBar mprogressBar4;
+    ProgressBar mprogressBar5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,26 +42,30 @@ public class Score extends AppCompatActivity implements AdapterView.OnClickListe
                     total=total +1;
                 }
         }
+        int total2 = 0;
         Map map2= pref2.getAll();
         for (Object key: map2.keySet()){
             if( (Boolean) map2.get((String) key).equals(true)){
-                total=total +1;
+                total2=total2 +1;
             }
         }
+        int total3 = 0;
         Map map3 = pref3.getAll();
         for (Object key: map3.keySet()){
             if( (Boolean) map3.get((String) key).equals(true)){
-                total=total +1;
+                total3=total3 +1;
             }
         }
+        int total4 = 0;
         Map map4= pref4.getAll();
         for (Object key: map4.keySet()){
             if( (Boolean) map4.get((String) key).equals(true)){
-                total=total +1;
+                total4=total4 +1;
             }
         }
 
-        this.mainScore = total;
+        int grantotal = total + total2 + total3 + total4;
+        this.mainScore = grantotal;
 
         long totalPerc = (long) Math.floor(100 * mainScore);
         long totalPerc1 = (long) Math.floor(totalPerc / 92 );
@@ -68,10 +77,35 @@ public class Score extends AppCompatActivity implements AdapterView.OnClickListe
         textView2.setText("Your Score is :" + mainScore );
 
         mprogressBar = (ProgressBar) findViewById(R.id.circular_progress_bar);
-        ObjectAnimator anim = ObjectAnimator.ofInt(mprogressBar, "progress", 0, mainScore);
+        ObjectAnimator anim = ObjectAnimator.ofInt(mprogressBar, "progress", 0, total);
         anim.setDuration(850);
         anim.setInterpolator(new DecelerateInterpolator());
         anim.start();
+
+        mprogressBar2 = (ProgressBar) findViewById(R.id.circular_progress_bar2);
+        ObjectAnimator anim2 = ObjectAnimator.ofInt(mprogressBar2, "progress", 0, total2);
+        anim2.setDuration(850);
+        anim2.setInterpolator(new DecelerateInterpolator());
+        anim2.start();
+
+        mprogressBar3 = (ProgressBar) findViewById(R.id.circular_progress_bar3);
+        ObjectAnimator anim3 = ObjectAnimator.ofInt(mprogressBar3, "progress", 0, total3);
+        anim3.setDuration(850);
+        anim3.setInterpolator(new DecelerateInterpolator());
+        anim3.start();
+
+        mprogressBar4 = (ProgressBar) findViewById(R.id.circular_progress_bar4);
+        ObjectAnimator anim4 = ObjectAnimator.ofInt(mprogressBar4, "progress", 0, total4);
+        anim4.setDuration(850);
+        anim4.setInterpolator(new DecelerateInterpolator());
+        anim4.start();
+
+        mprogressBar5 = (ProgressBar) findViewById(R.id.progress_bar);
+        ObjectAnimator anim5 = ObjectAnimator.ofInt(mprogressBar5, "progress", 0, mainScore);
+        anim5.setDuration(850);
+        anim5.setInterpolator(new DecelerateInterpolator());
+        anim5.start();
+
     }
 
 
