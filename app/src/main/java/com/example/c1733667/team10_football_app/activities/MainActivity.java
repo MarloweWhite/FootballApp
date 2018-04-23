1d90200b2a3c8abb48fd69fd0c7f4a25e9832dab
Pedro Gomes <GomesPO@cardiff.ac.uk>
2018-04-23 14:17:01 +0100

    Notification Service

--- app/src/main/java/com/example/c1733667/team10_football_app/activities/MainActivity.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/activities/MainActivity.java
@@ -120,6 +120,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         ButtonClass achievementButton = new ButtonClass("Achievement Button");
         achievementButton.achievementButtonListener(btnAchievement = findViewById(R.id.btnAchievement),this);
+
+        setContentView(R.layout.activity_main);
+
+        startService(new Intent(this, NotificationService.class));
 }
 
 
@@ -127,5 +131,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return true;
     }
+
+
+
 
 }
