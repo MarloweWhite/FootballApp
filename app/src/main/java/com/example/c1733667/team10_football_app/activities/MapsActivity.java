971de2966db4b0df840c5d11b46b9f05d631dc33
Pedro Gomes <GomesPO@cardiff.ac.uk>
2018-04-24 15:32:17 +0100

    share screen shot code

--- app/src/main/java/com/example/c1733667/team10_football_app/activities/MapsActivity.java
+++ app/src/main/java/com/example/c1733667/team10_football_app/activities/MapsActivity.java
@@ -1,18 +1,30 @@
 package com.example.c1733667.team10_football_app.activities;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.c1733667.team10_football_app.BuildConfig;
 import com.example.c1733667.team10_football_app.R;
 import com.example.c1733667.team10_football_app.classpack.Navigation;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,6 +37,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -47,6 +63,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     private SharedPreferences pref3;
     private SharedPreferences pref4;
     private Intent intent;
+    private File imagePath;
+    private Button share;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -102,7 +120,73 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
+        share = (Button)findViewById(R.id.share);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bitmap bitmap = takeScreenshot();
+
+                System.out.println("############################################"+ bitmap.getHeight());
+
+                saveBitmap(bitmap);
+                shareIt();
+            }
+        });
     }
+
+        public Bitmap takeScreenshot() {
+            View rootView = findViewById(android.R.id.content).getRootView();
+            rootView.setDrawingCacheEnabled(true);
+            return rootView.getDrawingCache();
+        }
+
+        public void saveBitmap(Bitmap bitmap) {
+            imagePath = new File(Environment.getExternalStorageDirectory() + "/screenshot.png");
+
+//            imagePath = new File(getApplicationContext().getFilesDir(), "screenshot.png");
+            FileOutputStream fos;
+
+//            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                    == PackageManager.PERMISSION_GRANTED) {
+//                System.out.println("################################ PERMISSION IS GRANTED ");
+//            }else{
+//                System.out.println("################################ PERMISSION NOT GRANTED");
+//                ActivityCompat.requestPermissions(this,
+//                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+//                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+//            }
+
+
+            try {
+                System.out.println("###################################"+ imagePath);
+                fos = new FileOutputStream(imagePath);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                fos.flush();
+                fos.close();
+                System.out.println("###################################"+ fos.);
+
+            } catch (FileNotFoundException e) {
+                Log.e("GREC", e.getMessage(), e);
+            } catch (IOException e) {
+                Log.e("GREC", e.getMessage(), e);
+            }
+        }
+
+        private void shareIt() {
+            Uri uri = FileProvider.getUriForFile(MapsActivity.this, BuildConfig.APPLICATION_ID + ".provider",imagePath);
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            sharingIntent.setType("image/*");
+            String shareBody = "In Tweecher, My highest score with screen shot";
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "My Tweecher score");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
+
+            startActivity(Intent.createChooser(sharingIntent, "Share via"));
+        }
+
+
 
     public void getClubInfo() {
         Resources res = getResources();
@@ -174,6 +258,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -240,6 +325,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(defaultMap, 6));
     }
+
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
